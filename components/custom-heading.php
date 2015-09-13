3f255b0a88ab33b7f2588625b559757f6280ba5b
Jordan Gonzales <jgonzo127@gmail.com>
2015-09-13 01:23:44 -0700

    Created widget options for heading text and heading level

--- components/custom-heading.php
+++ components/custom-heading.php
@@ -8,6 +8,62 @@
  * @since   1.0.0
  */
 
+class MM_Custom_Heading_Widget extends WP_Widget {
+
+	function mm_custom_heading_widget() {
+		// Instantiate the parent object
+		parent::__construct( false, 'MM Custom Heading' );
+	}
+
+	function widget( $args, $instance ) {
+		// Widget output
+
+		extract( $args );
+		$title = apply_filters( 'widget_title', $instance[ 'title' ] );
+		$num_badges = $instance['num_badges'];
+		$display_tooltip = $instance['display_tooltip'];
+
+		$options = get_option( 'wptreehouse_badges') ;
+		$wptreehouse_profile = $options['wptreehouse_profile'];
+	
+		require( 'inc/front-end.php');
+
+	}
+
+	function update( $new_instance, $old_instance ) {
+		// Save widget options
+		
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['num_badges'] = strip_tags($new_instance['num_badges']);
+		$instance['display_tooltip'] = strip_tags($new_instance['display_tooltip']);
+
+		return $instance;
+
+	}
+
+	function form( $instance ) {
+		// Output admin widget options form
+
+		$title = esc_attr( $instance['title'] );
+		$num_badges = esc_attr( $instance['num_badges'] );
+		$display_tooltip = esc_attr( $instance['display_tooltip'] );
+
+		$options = get_option( 'wptreehouse_badges') ;
+		$wptreehouse_profile = $options['wptreehouse_profile'];
+
+		require( 'inc/widget-fields.php' );
+	}
+}
+
+function wptreehouse_badges_register_widgets() {
+	register_widget( 'mm_custom_heading_widget' );
+}
+
+add_action( 'widgets_init', 'wptreehouse_badges_register_widgets' );
+
+
+
 add_shortcode( 'mm_custom_heading', 'mm_custom_heading_shortcode' );
 /**
  * Output Custom Heading.
